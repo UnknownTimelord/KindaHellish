4e63ad532530c34cea80ca604ee24e6f87c48f20
Tenth <amysterioustimelord@gmail.com>
2022-04-19 08:59:07 -0600

    Initial commit

---
+++ src/main/java/net/tenth/kindahellish/KindaHellish.java
@@ -0,0 +1,14 @@
+package net.tenth.kindahellish;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KindaHellish implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("kindahellish");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
